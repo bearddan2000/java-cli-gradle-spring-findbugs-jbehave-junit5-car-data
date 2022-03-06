b1233c57a7b85d6cf22162e79ae004825abf7db6
bearddan2000 <bearddan2000@yahoo.com>
2022-03-05 20:14:06 -0500

    initial commit

---
+++ bin/src/test/java/example/CarAttributeTest.java
@@ -0,0 +1,41 @@
+package example;
+
+import org.jbehave.core.annotations.Given;
+import org.jbehave.core.annotations.Then;
+import org.jbehave.core.annotations.When;
+
+import org.junit.jupiter.api.Assertions;
+
+public class CarAttributeTest {
+
+  example.service.CarService service;
+
+  example.model.Car car;
+
+  @When("Car created")
+  public void createCar() {
+     service = new example.service.CarServiceImpl();
+     car = service.createCar();
+  }
+
+  @Then("Car price should be 0.0")
+  public void checkPrice() {
+     Assertions.assertEquals(car.getPrice(), 0.0);
+  }
+
+  @Then("Car model should be test")
+  public void checkModel() {
+     Assertions.assertEquals(car.getModel(), "test");
+  }
+
+  @Then("Car year should be 0")
+  public void checkYear() {
+     Assertions.assertEquals(car.getYear(), 0);
+  }
+
+  @Then("Car make should be test")
+  public void checkMake() {
+    Assertions.assertEquals(car.getMake(), "test");
+  }
+
+}
